11d191fc1341f7291c598ac5863b5751c058d5b0
Tertulianonews <tertulianobarbos@hotmail.com>
2025-08-14 20:43:11 -0300

    Atualização do site

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -47,7 +47,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4198ee749aa947090a4e5a16ab4656ac
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "68fd4a8306cbede49c6fd6e9824ebedd",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6ec87339cd1798b1569cc28788269c5f",
+"flutter_bootstrap.js": "a4ced78936870e752b082cfaf7d3f7f6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
